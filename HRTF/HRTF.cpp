ce6670cad27ad8e92a57b415e6ad2b68c05ea95f
haru19970409 <haru19970409@gmail.com>
2020-08-26 13:32:26 +0900

    playaudio

--- HRTF/HRTF.cpp
+++ HRTF/HRTF.cpp
@@ -17,9 +17,9 @@
 #define AMPLITUDE 1.0// 16bit
 #pragma warning(disable : 4996)
 
-int applyHRTF(MONO_PCM source, int deg, int elev)
+/*int applyHRTF(MONO_PCM source, int deg, int elev)
 {
-    STEREO_PCM appliedSource; /* ステレオの音データ */
+    STEREO_PCM appliedSource; // ステレオの音データ
     SF_INFO sfinfo;
     SF_INFO Lsfinfo;
     SF_INFO Rsfinfo;
@@ -116,7 +116,7 @@ int applyHRTF(MONO_PCM source, int deg, int elev)
         fftwf_execute(plan);
         if (plan) fftwf_destroy_plan(plan);
 
-        /* ここに処理を書く */
+        // ここに処理を書く 
         for (int j = 0; j < FFTSIZE; j++) {
             std::complex<float> s(Ldst[j][0], Ldst[j][1]);
             std::complex<float> l(FFTleft[j][0], FFTleft[j][1]);
@@ -148,23 +148,23 @@ int applyHRTF(MONO_PCM source, int deg, int elev)
 
     }
 
-    appliedSource.fs = SAMPLE_RATE; /* 標本化周波数 */
-    appliedSource.bits = 16; /* 量子化精度 */
-    appliedSource.length = sfinfo.frames; /* 音データの長さ */
-    appliedSource.sL = (double*)calloc(appliedSource.length, sizeof(double)); /* メモリの確保 */
-    appliedSource.sR = (double*)calloc(appliedSource.length, sizeof(double)); /* メモリの確保 */
+    appliedSource.fs = SAMPLE_RATE; // 標本化周波数
+    appliedSource.bits = 16; // 量子化精度
+    appliedSource.length = sfinfo.frames; // 音データの長さ 
+    appliedSource.sL = (double*)calloc(appliedSource.length, sizeof(double));  // メモリの解放 
+    appliedSource.sR = (double*)calloc(appliedSource.length, sizeof(double));  // メモリの解放 
     for (int n = 0; n < appliedSource.length; n++)
     {
         appliedSource.sL[n] = Loutdata[n];
         appliedSource.sR[n] = Routdata[n];
     }
     //ここで音声出力bufferに流せばよい？
-    stereo_wave_write(&appliedSource, (char*)generatename); /* WAVEファイルにステレオの音データを出力する */
-    free(appliedSource.sL); /* メモリの解放 */
-    free(appliedSource.sR); /* メモリの解放 */
+    stereo_wave_write(&appliedSource, (char*)generatename); // WAVEファイルにステレオの音データを出力する 
+    free(appliedSource.sL); // メモリの解放 
+    free(appliedSource.sR); // メモリの解放 
 
 
-    /*// 入出力配列の値をcsvファイルに保存
+    // 入出力配列の値をcsvファイルに保存
     std::fstream fs_src("src.csv", std::ios::out);
     std::fstream fs_dst("dst.csv", std::ios::out);
     std::fstream fs_out("output.csv", std::ios::out);
@@ -178,7 +178,7 @@ int applyHRTF(MONO_PCM source, int deg, int elev)
         fs_src << src[i][0] << "," << src[i][1] << std::endl;
         fs_dst << FFTleft[i][0] << "," << FFTleft[i][1] << std::endl;
         fs_out << Loutdata[i] << "," << Ldst2[i][1] << std::endl;
-    }*/
+    }
 
     // 終了時、専用関数でメモリを開放する
     fftw_free(src);
@@ -198,219 +198,93 @@ int applyHRTF(MONO_PCM source, int deg, int elev)
 
 
     return 0;
-}
+}*/
 
-int main(void)
+/*int main(void)
 {
     printf("START!!!\n");
-    MONO_PCM pcm0; /* モノラルの音データ */
+    MONO_PCM pcm0; // モノラルの音データ
 
-    mono_wave_read(&pcm0, (char*)"sample08.wav"); /* WAVEファイルからモノラルの音データを入力する */
+    mono_wave_read(&pcm0, (char*)"sample08.wav");
     applyHRTF(pcm0, 10, 5);
 
-    free(pcm0.s); /* メモリの解放 */
+    free(pcm0.s); // メモリの解放
 
     return 0;
-}
-
-/*#pragma once
-#include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "wavlib.h"
-#define BUFFER_COUNT 2 // マルチバッファ数
-
-LRESULT CALLBACK MainWndProc(HWND hwnd, UINT Message,
-    WPARAM wParam, LPARAM lParam);
-
-static const char* lpClassName = "My Class";
-static const char* lpClassTitle = "First WindowsApplication";
-static const char* lpMenuName = "My Menu";
-
-wav_head bufhead;
-static char InFileN[] = { "C:\\Users\\haru1\\source\\repos\\HRTF\\x64\\Debug\\fot.bak.441.wav" };
-int bufferSize;    // waveバッファサイズ(byte)
-int bufferLength;  // waveバッファ長(データ数/s)
-int bufferSelect;  // バッファ選択番号
-static short* (wWave[BUFFER_COUNT]); // waveバッファ(マルチ)
-
-static HWAVEOUT hWave;
-static WAVEHDR whdr[BUFFER_COUNT];
-int waveDataSize;  // 再生予定のWAVEデータサイズ
-int readDataSize;  // 再生済のWAVEデータサイズ
-int initWave(HWND hWnd);
-void closeWave(void);
-LRESULT wmWomDone(void);
+}*/
 
 
 
+#include<stdio.h>
+#include<math.h>
+#include"portaudio.h"
+#define Fs 44100 //サンプリング周波数
+#define FRAMES_PER_BUFFER 128 //バッファサイズ
+#define pi 3.14159265358979323
 
-/* -----------
-   Main Program
-    ------------ */
-    /*int main(HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        LPSTR lpszCmdLine, INT nCmdShow)
-    {
-        HWND hWndMain;
-        MSG msg;
-        WNDCLASSEX   wndclass;
+/*ユーザ定義データ*/
+typedef struct {
+    float freq; //正弦波の周波数
+    float index;
+}padata;
 
-        wndclass.cbSize = sizeof(WNDCLASSEX);
-        wndclass.hInstance = hInstance;
-        wndclass.lpszClassName = lpClassName;
-        wndclass.lpfnWndProc = MainWndProc;
-        wndclass.style = 0;
-        wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-        wndclass.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
-        wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-        wndclass.lpszMenuName = NULL;
-        wndclass.cbClsExtra = wndclass.cbWndExtra = 0;
-        wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-        if (!RegisterClassEx(&wndclass)) {
-            MessageBox(0, "エラー：ウィンドウ未登録", "MyApp", MB_OK);
-            return FALSE;
-        }
-        if (!(hWndMain =
-            CreateWindow(lpClassName, lpClassTitle, WS_OVERLAPPEDWINDOW,
-                CW_USEDEFAULT, CW_USEDEFAULT, 100, 100,
-                HWND_DESKTOP, NULL, hInstance, NULL)))
-            return FALSE;
+/* オーディオ処理コールバック関数*/
+static int dsp(const void* inputBuffer, //入力
+    void* outputBuffer, //出力
+    unsigned long framesPerBuffer,
+    const PaStreamCallbackTimeInfo* timeInfo,
+    PaStreamCallbackFlags statusFlags,
+    void* userData //ユーザ定義データ 
+) {
+    padata* data = (padata*)userData;
+    float* out = (float*)outputBuffer;
+    long i;
 
-        ShowWindow(hWndMain, nCmdShow);
-        UpdateWindow(hWndMain);
-        if (initWave(hWndMain) == FALSE)return FALSE; // wave初期化
-        while (GetMessage(&msg, NULL, 0, 0)) {
-            DispatchMessage(&msg);
-        }
-        closeWave(); // 後処理
-        return msg.wParam;
+    for (i = 0; i < framesPerBuffer; i++) {
+        *out++ = 0.7 * sin(2 * pi * data->freq * data->index / Fs); //チャンネル1（左）
+        *out++ = 0.7 * sin(2 * pi * data->freq * data->index / Fs); //チャンネル2（右）
+        data->index += 1.f;
     }
-    /* ------------------
-        Window Procedure
-       ------------------*/
-       /*LRESULT CALLBACK MainWndProc(HWND hwnd, UINT Message,
-           WPARAM wParam, LPARAM lParam)
-       {
-           switch (Message) {
+    return 0;
+}
+int main(void) {
+    PaStreamParameters outParam; //出力の定義
+    PaStream* stream;
+    PaError err;
+    padata data; //ユーザ定義データ
+    data.freq = 800.f;
+    data.index = 0.f;
 
-           case MM_WOM_DONE:// 1つのバッファのデータ再生終了時に呼び出される
-               return wmWomDone();
-           case WM_DESTROY:
-               PostQuitMessage(0);
-               break;
-           default:
-               return DefWindowProc(hwnd, Message, wParam, lParam);
-           }
-           return FALSE;
-       }
-       /* ----------------------------------------
-        Initialization of Wave relevant procedure
-        ------------------------------------------ */
-        /*int initWave(HWND hWnd)
-        {
-            int i;
-            WAVEFORMATEX wfe;
+    //PortAudio初期化
+    Pa_Initialize();
 
-            wavread(&bufhead, InFileN);
-            bufferLength =
-                bufhead.fs * bufhead.Nch;      // fs * channelデータ数/s
-            bufferSize =
-                bufferLength * sizeof(short);  // バッファサイズ〔byte〕
-            for (i = 0; i < BUFFER_COUNT; i++) {
-                wWave[i] =
-                    (short*)malloc(bufferSize); // wWaveに領域割り当て
-            }
+    //出力の設定
+    outParam.device = Pa_GetDefaultOutputDevice(); //デフォルトのオーディオデバイス
+    outParam.channelCount = 2;
+    outParam.sampleFormat = paFloat32; //32bit floatで処理
+    outParam.suggestedLatency = Pa_GetDeviceInfo(outParam.device)->defaultLowOutputLatency;
+    outParam.hostApiSpecificStreamInfo = NULL;
 
-            // 出力デバイスの設定
-            wfe.wBitsPerSample = bits;  // bits is in wavlib.h
-            wfe.nChannels = bufhead.Nch;
-            wfe.wFormatTag = WAVE_FORMAT_PCM;
-            wfe.nSamplesPerSec =
-                (int)bufhead.fs;          // fs　サンプリング周波数〔Hz〕
-            wfe.nBlockAlign = wfe.nChannels * wfe.wBitsPerSample / 8;
-            wfe.nAvgBytesPerSec = wfe.nSamplesPerSec * wfe.nBlockAlign;
-            wfe.cbSize = waveDataSize = bufhead.Nbyte;
+    //PortAudioオープン
+    Pa_OpenStream(
+        &stream,
+        NULL,
+        &outParam,
+        Fs,
+        FRAMES_PER_BUFFER,
+        paClipOff,
+        dsp,
+        &data);
 
-            waveOutOpen(&hWave, WAVE_MAPPER, &wfe,   // 出力デバイスを開く
-                (DWORD)hWnd, 0, CALLBACK_WINDOW);
+    //PortAudioスタート
+    Pa_StartStream(stream);
 
-            for (i = 0; i < BUFFER_COUNT; i++) {
-                // 再生バッファにデータを送るための設定
-                whdr[i].lpData = (LPSTR)wWave[i];
-                whdr[i].dwBufferLength = 0;
-                whdr[i].dwFlags = WHDR_BEGINLOOP | WHDR_ENDLOOP;
-                whdr[i].dwLoops = 1;
+    //エンターキーが押されるまで待機
+    getchar();
 
-                waveOutPrepareHeader(hWave, &(whdr[i]), sizeof(WAVEHDR));
-            }
-
-            readDataSize = 0;
-            // ダブルバッファリングをするために空データを書き込み
-            waveOutWrite(hWave, &(whdr[0]), sizeof(WAVEHDR));
-            waveOutWrite(hWave, &(whdr[1]), sizeof(WAVEHDR));
-
-            // バッファ番号の設定
-            bufferSelect = 2 % BUFFER_COUNT;
-            return TRUE;
-        }
-        /* ------------
-           Waveの後処理
-           ------------*/
-           /*void closeWave(void)
-           {
-               int i;
-               waveOutReset((HWAVEOUT)hWave);
-               for (i = 0; i < BUFFER_COUNT; i++) {
-                   waveOutUnprepareHeader((HWAVEOUT)hWave, &(whdr[i]),
-                       sizeof(WAVEHDR));
-                   free(wWave[i]);
-               }
-               free(bufhead.data);
-           }
-           /* -----------------------
-           バッファの終了時に呼ばれる
-             ---------------------- */
-             /*LRESULT wmWomDone(void)
-             {
-                 int readsize, i, idlast;
-                 // 最後まで再生した場合は最初にメモリのポインタを戻す
-                 if (waveDataSize - readDataSize == 0) {
-                     readDataSize = 0;
-                     // MM_WOM_DONEをおこすためにwaveOutWriteを呼ぶ
-                     whdr[bufferSelect].dwBufferLength = 0;
-                     waveOutWrite((HWAVEOUT)hWave, &(whdr[bufferSelect]),
-                         sizeof(WAVEHDR));
-
-                     bufferSelect =
-                         (bufferSelect + 1) % BUFFER_COUNT; // バッファのチャネル選択
-                     return 0;
-                 }
-
-                 readsize = bufferSize;  // 読み込みサイズ
-                 if (readsize > waveDataSize - readDataSize) {
-                     readsize = waveDataSize - readDataSize;
-                     idlast = 1;
-                 }
-                 else {
-                     idlast = 0;
-                 }
-                 /* -------------------------------------------------------
-                  読み込み bufhead.data に書き込むか，wWave[bufferSelect]に
-                  直接データを書き込む
-                   ------------------------------------------------------- */
-                   /*for (i = 0; i < readsize / 2; i++) {
-                       wWave[bufferSelect][i] = bufhead.data[readDataSize / 2 + i];
-                   }
-                   if (idlast == 1) readDataSize = waveDataSize;
-                   else          readDataSize += readsize;
-                   // 再生バッファに書き込み
-                   whdr[bufferSelect].dwBufferLength = readsize;
-                   waveOutWrite((HWAVEOUT)hWave, &(whdr[bufferSelect]),
-                       sizeof(WAVEHDR));
-
-                   bufferSelect =
-                       (bufferSelect + 1) % BUFFER_COUNT;   // バッファ番号の更新
-                   return 0;
-               }*/
-
+    //PortAudio終了
+    Pa_StopStream(stream);
+    Pa_CloseStream(stream);
+    Pa_Terminate();
+    return 0;
+}
---
+++ HRTF/include/resource.h
@@ -0,0 +1,5 @@
+#define	IDC_RADIOBOX	100
+
+#define	IDC_PLAY_BUTTON		200
+#define	IDC_STOP_BUTTON		201
+#define	IDC_GROUPBOX104		202
